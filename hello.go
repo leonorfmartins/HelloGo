79a9a7fb697f358c07dcfb987450d2d50061ad7d
Leonor Martins <leonorfmartins@gmail.com>
2017-07-01 19:36:09 +0200

    Add first library to reverse a string

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/leonorfmartins/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world. \n")
+	fmt.Printf(stringutil.Reverse("Hello, world. \n"))
 }
